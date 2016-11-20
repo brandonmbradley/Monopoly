ef75515dbbfb45ab878e8dd0320774ef2779bc6c
pbadgley <sailing254@gmail.com>
2016-11-20 14:50:27 -0500

    Update 3
    Added no online mouseover text and polish on left side UI

--- Assets/AppearProperties.cs
+++ Assets/AppearProperties.cs
@@ -2,37 +2,33 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class AppearScript : MonoBehaviour
+public class AppearProperties: MonoBehaviour
 {
 
-    public GameObject rent;
     public GameObject hover;
     public GameObject properties;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-    rent.gameObject.SetActive(false);
-    hover.gameObject.SetActive(true);
-    properties.gameObject.SetActive(false);
+        hover.gameObject.SetActive(true);
+        properties.gameObject.SetActive(false);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
 
     }
 
     public void renton()
     {
-        rent.gameObject.SetActive(true);
         properties.gameObject.SetActive(true);
         hover.gameObject.SetActive(false);
     }
 
     public void rentoff()
     {
-        rent.gameObject.SetActive(false);
         properties.gameObject.SetActive(false);
         hover.gameObject.SetActive(true);
     }
