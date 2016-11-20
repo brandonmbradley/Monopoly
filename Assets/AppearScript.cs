fe0b010e1c27cc38ee15b6ea89fefdbda40544fb
pbadgley <sailing254@gmail.com>
2016-11-20 01:06:01 -0500

    Revision 2
    Added More UI elements to the main game

---
+++ Assets/AppearScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AppearScript : MonoBehaviour
+{
+
+    public GameObject rent;
+
+	// Use this for initialization
+	void Start ()
+    {
+    rent.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    public void renton()
+    {
+        rent.gameObject.SetActive(true);
+    }
+
+    public void rentoff()
+    {
+        rent.gameObject.SetActive(false);
+    }
+}
